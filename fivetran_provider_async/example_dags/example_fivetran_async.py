a6fa2d1f3c33b5c90c7f9ae64890b11aa0928dd2
Phani Kumar <phani.kumar@astronomer.io>
2022-11-30 13:28:44 +0530

    Apply review suggestions

--- fivetran_provider_async/example_dags/example_fivetran_async.py
+++ fivetran_provider_async/example_dags/example_fivetran_async.py
@@ -9,6 +9,7 @@ from fivetran_provider_async.sensors import FivetranSensorAsync
 default_args = {
     "owner": "Airflow",
     "start_date": datetime(2022, 4, 14),
+    "fivetran_conn_id": "fivetran_default",
 }
 
 dag = DAG(
@@ -21,19 +22,16 @@ dag = DAG(
 with dag:
     fivetran_async_op = FivetranOperatorAsync(
         task_id="fivetran_async_op",
-        fivetran_conn_id="fivetran_default",
         connector_id="bronzing_largely",
     )
 
     fivetran_sync_op = FivetranOperator(
         task_id="fivetran_sync_op",
-        fivetran_conn_id="fivetran_default",
         connector_id="bronzing_largely",
     )
 
     fivetran_async_sensor = FivetranSensorAsync(
         task_id="fivetran_async_sensor",
-        fivetran_conn_id="fivetran_default",
         connector_id="bronzing_largely",
         poke_interval=5,
         xcom="{{ task_instance.xcom_pull('fivetran_sync_op', key='return_value') }}",
