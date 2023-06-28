c863ed84f83f3b6073ded50dc8f9602363ea20a7
Pankaj <pankaj.singh@astronomer.io>
2023-06-28 20:29:56 +0530

    Move fivetran sync provider code to this repo

--- fivetran_provider_async/example_dags/example_fivetran_async.py
+++ fivetran_provider_async/example_dags/example_fivetran_async.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timedelta
 
 from airflow import DAG
-from fivetran_provider.operators.fivetran import FivetranOperator
+from fivetran_provider_async.operators import FivetranOperator
 
 from fivetran_provider_async.operators import FivetranOperatorAsync
 from fivetran_provider_async.sensors import FivetranSensorAsync
