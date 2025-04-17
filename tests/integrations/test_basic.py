6ffacf14228126a3c0d221a150b8534db9cbb7f0
Pankaj Singh <98807258+pankajastro@users.noreply.github.com>
2025-04-17 14:38:36 +0530

    Update tests/integrations/test_basic.py

--- tests/integrations/test_basic.py
+++ tests/integrations/test_basic.py
@@ -3,7 +3,7 @@ from airflow.providers_manager import ProvidersManager
 
 
 @pytest.mark.integration
-def test_fivetran_conn():
+def test_fivetran_provider_installation():
 
     manager = ProvidersManager()
     assert "airflow-provider-fivetran-async" in manager.providers.keys()
